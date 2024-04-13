7abcf6dd1aeb52014e47fb8c387ec5d99e28b20d
Kathik1772 <karthikamma2004@gmail.com>
2024-04-13 16:01:09 +0530

    pending

--- Frontend/lib/pages/profile/profile.dart
+++ Frontend/lib/pages/profile/profile.dart
@@ -12,13 +12,45 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('PROFILE        ',
+        title: Center(child: Text('PERSONAL DETAILS      ',
         style: GoogleFonts.varelaRound(
                     color: Colors.white,
                     fontSize: 25,
                     fontWeight: FontWeight.bold)
         )),
         backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        children: [
+          Container(
+              margin: const EdgeInsets.only(
+                  left: 10, right: 10, top: 5, bottom: 10),
+                child: Card(
+                  elevation: 10,
+                  surfaceTintColor: Colors.transparent,
+                  child: ListTile(
+                    leading:  CircleAvatar(
+                      backgroundImage:AssetImage('assets/download.jpg'),
+                      radius: 30,
+                    ),
+                    title: Text(
+                      'KARTHIK S KASHYAP',
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                      style: GoogleFonts.varelaRound(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      '4AL22EC032',
+                      style: GoogleFonts.varelaRound(
+                          color: const Color.fromARGB(255, 0, 0, 0), fontSize: 18),
+                    ),
+                  ),
+                ),
+            )
+        ]
       ),
     );
   }
