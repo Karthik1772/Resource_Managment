eb04a45127f6049e99da3f15400ea8f52cebb643
Karthik1772 <karthikamma2004@gmail.com>
2024-04-22 13:24:02 +0530

    pending

--- Backend/router/router.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const router=express.Router();
-router.post("/about",require("../controllers/registercontroller"));
-router.post("/login",require("../controllers/logincontroller"));
-module.exports=router;
-
-    
