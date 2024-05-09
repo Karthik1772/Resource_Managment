712fdfbc98f853589afa17a0e09ee829b78a289a
Karthik1772 <karthikamma2004@gmail.com>
2024-05-09 18:50:18 +0530

    pending

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
-import 'package:alvas_mangment/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
-
 import 'generated_routes.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
   runApp(const MyApp());
 }
 
